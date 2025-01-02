63d69557f4c7674eca24e320633d418bed61154a
bishalqx980 <osamamohamedmohamed30@gmail.com>
2025-01-02 19:00:54 +0200

    Update Voice Transcription

--- src/main.py
+++ src/main.py
@@ -63,4 +63,4 @@ app.include_router(voice.voice_router)
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
 
-# uvicorn main:app --host localhost --port 8080 --reload
+# uvicorn main:app --host localhost --port 8000 --reload
